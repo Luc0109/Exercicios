b7119e7f1a306086d96907cc8da3dcb645848c87
LUCAS FERREIRA JOAQUIM <37710938809@info.local>
2024-05-02 17:26:22 -0300

    feat:Exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,20 @@
+// Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+// nome de usuário é &quot;admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+// foi bem-sucedido ou não.
+let usuário = prompt("Coloque seu usuario: ")
+let senha = prompt("Digite sua senha: ")
+
+console.log("Seja bem vindo " + usuário + "!!")
+console.log(senha)
+
+if (usuário == "admin") {
+    console.log("Você é Admin")
+
+   if (senha == "senha123") {
+        console.log("Seja bem vindo ADMIN!");
+   }
+
+} else {
+    console.log("Você não tem permissçao de acesso!")
+}
+
--- script.js
+++
@@ -1,8 +0,0 @@
-alert("Hello Word!");
-
-console.log("Essa mensagem aparecera no console");
-let pontos = 100
-
-let bonus = parseInt(prompt("Quantos pontos você deseja somar?"))
-
-console.log(pontos + bonus)
