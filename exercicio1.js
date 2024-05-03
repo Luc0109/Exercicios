b6420c66c837a9f0b0872fa9ff3de076cf2d3b44
Luc0109 <166746452+Luc0109@users.noreply.github.com>
2024-05-03 16:55:27 -0300

    Update exercicio4.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,12 +1,18 @@
-// Faça um programa que receba um número do usuário e informe se este número é par ou
-// ímpar.
+// Faça um programa que receba 4 notas de um aluno, calcule e imprima a média aritmética das
+// notas e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO para
+// notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
 
-let numero = prompt ("Coloque o número de usuário: ")
+let n1 = parseFloat(prompt("Coloque a nota: "))
+let n2 = parseFloat(prompt("Coloque a nota: "))
+let n3 = parseFloat(prompt("Coloque a nota: "))
+let n4 = parseFloat(prompt("Coloque a nota: "))
 
-if (numero % 2 == 0){
-    alert ("Par")
+media = (n1 + n2 + n3 + n4) / 4
 
-}
-if(numero % 2 == 1){
-    alert ("Impar")
-}
+if (media >= 7) {
+    alert("Aprovado");
+
+} else if (media >= 5){
+    alert("Recuperação"); 
+} else {
+    alert ("Reprovado")
