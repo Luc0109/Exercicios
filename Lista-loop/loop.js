3ed9bc3cb5ff1c36f2ccae8f8258fa3e3f8bba78
LUCAS FERREIRA JOAQUIM <37710938809@info.local>
2024-05-07 17:29:57 -0300

    loop

---
+++ Lista-loop/loop.js
@@ -0,0 +1,26 @@
+// //1. Imprimir números de 1 a 10 utilizando o loop for//
+
+// for (let contador = 1; contador <= 10; contador++) {
+  
+  
+//     console.log(contador);
+// }
+
+//2. Imprimir números pares de 1 a 10.//
+
+// for (let contador = 2; contador <= 10; contador+=2) {
+  
+
+//         console.log(contador)
+//     }
+
+    for (let contador = 1; contador <= 50; contador+=2) {
+  
+
+        console.log(contador)
+    }
+    
+
+
+
+
---
+++ lista 1/script.js
@@ -0,0 +1,12 @@
+// Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
+// crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
+// não.
+
+let n1 = prompt("Número: ")
+let n2 = prompt("Número: ")
+let n3 = prompt("Número: ")
+
+
+
+alert("Não esta em ordem crescente")
+
