01de4a12bd4afad68ae723d4762880e00cf4572d
LUCAS FERREIRA JOAQUIM <37710938809@info.local>
2024-05-08 16:58:03 -0300

    loop

--- Lista-loop/loop.js
+++ Lista-loop/loop.js
@@ -1,26 +1,89 @@
-// //1. Imprimir números de 1 a 10 utilizando o loop for//
+// // // // // // // // //1. Imprimir números de 1 a 10 utilizando o loop for//
 
-// for (let contador = 1; contador <= 10; contador++) {
+// // // // // // // // for (let contador = 1; contador <= 10; contador++) {
   
   
-//     console.log(contador);
+// // // // // // // //     console.log(contador);
+// // // // // // // // }
+
+// // // // // // // //2. Imprimir números pares de 1 a 10.//
+
+// // // // // // // // for (let contador = 2; contador <= 10; contador+=2) {
+  
+
+// // // // // // // //         console.log(contador)
+// // // // // // // //     }
+
+// // // // // // // 3. Faça um programa que imprima os números ímpares de 0 a 50;
+
+// // // // // // //     for (let contador = 1; contador <= 50; contador+=2) {
+  
+
+// // // // // // //         console.log(contador)
+// // // // // // //     }
+    
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+// let numero = parseInt(prompt("Qual tabuada quer ver?(De 1 a 10)"))
+
+
+
+// if (!isNaN(numero)) {
+//     console.log("Tabuada do " + numero + ":")
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(numero + " x " + i + " = " + numero * i)
+//     }
+// }
+// else {
+//     console.log("Por favor, insira um número válido.")
 // }
 
-//2. Imprimir números pares de 1 a 10.//
+// // // // // / Imprimir os primeiros 10 números da sequência de Fibonacci.
 
-// for (let contador = 2; contador <= 10; contador+=2) {
-  
 
-//         console.log(contador)
-//     }
+// // // // let fibonacci = [0, 1]; 
 
-    for (let contador = 1; contador <= 50; contador+=2) {
-  
+// // // // for (let i = 2; i < 10; i++) {
+// // // //   fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+// // // // }
 
-        console.log(contador)
-    }
+// // // // console.log('Os 10 números da sequência de Fibonacci são:');
+// // // // fibonacci.forEach(num => console.log(num));
+
+// // // 6. Imprimir números pares de 0 a 20 utilizando o loop while
+
+// // let numero = 0;
+
+// // while (numero <= 20) {
+// //   if (numero % 2 === 0) {
+// //     console.log(numero);
+// //   }
+// //   numero++;
+// // }
+// // 7.Calcular a soma dos números de 1 a 100 utilizando a estrutura  while.
+
+// let soma = 0;
+// let numero = 1;
+
+// while (numero <= 100) {
+//   soma += numero;
+//   numero++;
+// }
+
+// console.log("A soma dos números de 1 a 100 é: " + soma);
+
+// 8. Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+
+ 
+let age = prompt("Qual sua idade?")
+
+if (age  <18) {
     
-
+}
 
 
 
