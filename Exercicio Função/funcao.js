f8043bb1adac1f0774ad2e407a21e47ea9f2b411
LUCAS FERREIRA JOAQUIM <37710938809@info.local>
2024-05-09 17:25:36 -0300

    Função

---
+++ Exercicio Função/funcao.js
@@ -0,0 +1,23 @@
+// Fazer um menu de exercícios utilizando exemplos dado em aula. Criar um menu, onde é possível executar 6 dos exercícios anteriores.
+// Cada exercício deve ser encapsulado em uma função, e o menu deve executar essas funções.
+// O usuário poderá escolher qual exercício ele quer visualizar.
+// O usuário deverá digitar 0 para sair do menu.     
+
+
+
+// let opcao = prompt("Escolha qual exercicio quer realizar: \n1 - Exemplo Hello Word\n2 - Exemplo repretição")
+
+// switch (opcao) {
+//     case "1":
+//         console.log("Hello Word")
+//         break;
+
+//         case "2":
+            
+//             break;
+//     default:
+//         break;
+// }
+
+let opcao = prompt("Escolha qual exercicio quer realizar: \n1 ")
+
